753cd0f58f15b3da7709f4c73cda6f873e0e4b51
Alex Kotov <kotovalexarian@gmail.com>
2020-12-30 01:34:55 +0500

    Add some options

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,34 @@ const CRATE_NAME: &str = env!("CARGO_PKG_NAME");
 struct Options {
     #[clap(about = "The name or IP address of the destination host")]
     host: String,
+
+    #[clap(
+        short = 'f',
+        long = "first",
+        default_value = "1",
+        about = "With what TTL to start",
+    )]
+    first_ttl: u8,
+
+    #[clap(
+        short = 'm',
+        long = "max-hops",
+        default_value = "30",
+        about = "The maximum number of hops to probe",
+    )]
+    max_ttl: u8,
+
+    #[clap(
+        short = 'q',
+        long = "queries",
+        default_value = "3",
+        about = "The number of probe packets per hop",
+    )]
+    nqueries: u32,
 }
 
 fn main() {
     let options = Options::parse();
 
-    println!("host: {:?}", options.host);
+    println!("{:?}", options);
 }
