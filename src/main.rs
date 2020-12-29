42558c521f599bc4bb4a89810e9cb590111bce5f
Alex Kotov <kotovalexarian@gmail.com>
2020-12-30 00:34:55 +0500

    Create Clap application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::{Clap};
+
+const CRATE_NAME: &str = env!("CARGO_PKG_NAME");
+
+#[derive(Debug, Clap)]
+#[clap(about, author, name = CRATE_NAME, version)]
+struct Options {
+    #[clap(about = "The name or IP address of the destination host")]
+    host: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let options = Options::parse();
+
+    println!("host: {:?}", options.host);
 }
