08fb8dd01eda46dfb0dfe81c28f55f27711803fa
Alex Kotov <kotovalexarian@gmail.com>
2020-12-31 13:43:06 +0500

    Try libc sockets

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::{Clap};
+use libc;
 
 const CRATE_NAME: &str = env!("CARGO_PKG_NAME");
 
@@ -37,4 +38,27 @@ fn main() {
     let options = Options::parse();
 
     println!("{:?}", options);
+
+    let socket = unsafe {
+        libc::socket(libc::AF_INET, libc::SOCK_RAW, libc::IPPROTO_ICMP)
+    };
+
+    let sockaddr = libc::sockaddr_in {
+        sin_family: libc::AF_INET as libc::sa_family_t,
+        sin_port: 0,
+        sin_addr: libc::in_addr {
+            s_addr: 0,
+        },
+        sin_zero: [0; 8],
+    };
+
+    let bind_result = unsafe {
+        libc::bind(
+            socket,
+            &sockaddr as *const libc::sockaddr_in as *const libc::sockaddr,
+            std::mem::size_of::<libc::sockaddr_in>() as libc::socklen_t
+        )
+    };
+
+    assert!(bind_result == 0);
 }
