9887859947bc1f3a6e7cc025b761a610efc3bdd1
Alex Kotov <kotovalexarian@gmail.com>
2021-01-01 09:03:56 +0500

    Move struct Options to separate file

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,8 @@
+mod options;
+
 use clap::{Clap};
 use libc;
-
-const CRATE_NAME: &str = env!("CARGO_PKG_NAME");
-
-#[derive(Debug, Clap)]
-#[clap(about, author, name = CRATE_NAME, version)]
-struct Options {
-    #[clap(about = "The name or IP address of the destination host")]
-    host: String,
-
-    #[clap(
-        short = 'f',
-        long = "first",
-        default_value = "1",
-        about = "With what TTL to start",
-    )]
-    first_ttl: u8,
-
-    #[clap(
-        short = 'm',
-        long = "max-hops",
-        default_value = "30",
-        about = "The maximum number of hops to probe",
-    )]
-    max_ttl: u8,
-
-    #[clap(
-        short = 'q',
-        long = "queries",
-        default_value = "3",
-        about = "The number of probe packets per hop",
-    )]
-    nqueries: u32,
-}
+use options::{Options};
 
 fn main() {
     let options = Options::parse();
---
+++ src/options.rs
@@ -0,0 +1,34 @@
+use clap::{Clap};
+
+const CRATE_NAME: &str = env!("CARGO_PKG_NAME");
+
+#[derive(Debug, Clap)]
+#[clap(about, author, name = CRATE_NAME, version)]
+pub struct Options {
+    #[clap(about = "The name or IP address of the destination host")]
+    host: String,
+
+    #[clap(
+        short = 'f',
+        long = "first",
+        default_value = "1",
+        about = "With what TTL to start",
+    )]
+    first_ttl: u8,
+
+    #[clap(
+        short = 'm',
+        long = "max-hops",
+        default_value = "30",
+        about = "The maximum number of hops to probe",
+    )]
+    max_ttl: u8,
+
+    #[clap(
+        short = 'q',
+        long = "queries",
+        default_value = "3",
+        about = "The number of probe packets per hop",
+    )]
+    nqueries: u32,
+}
