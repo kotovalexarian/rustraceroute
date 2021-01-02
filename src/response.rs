164283bf01315602a391a5eb4b71f5a1835fc0ba
Alex Kotov <kotovalexarian@gmail.com>
2021-01-02 06:46:15 +0500

    Add tests for struct Response

--- src/response.rs
+++ src/response.rs
@@ -24,3 +24,44 @@ impl Response {
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::net::{IpAddr, Ipv4Addr};
+    use super::*;
+
+    #[test]
+    fn parse() {
+        let ipv4_addr = Ipv4Addr::new(127, 0, 0, 1);
+        let ip_addr = IpAddr::V4(ipv4_addr);
+        let source = SockaddrInx::from_ip_addr(ip_addr);
+
+        let body: [u8; 56] = [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            123,
+            231,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            123, 231, // 31_719
+            231, 123, // 59_259
+        ];
+
+        let response = Response::parse(&body, &source).unwrap();
+
+        assert_eq!(response.type_,    123);
+        assert_eq!(response.code,     231);
+        assert_eq!(response.ident,    31_719);
+        assert_eq!(response.sequence, 59_259);
+
+        match response.source {
+            SockaddrInx::V6(_) => panic!(),
+            SockaddrInx::V4(sockaddr_in) => {
+                assert_eq!(sockaddr_in.sin_family,
+                           libc::AF_INET as libc::sa_family_t);
+                assert_eq!(sockaddr_in.sin_port, 0);
+                assert_eq!(sockaddr_in.sin_addr.s_addr, 2_130_706_433);
+                assert_eq!(sockaddr_in.sin_zero, [0, 0, 0, 0, 0, 0, 0, 0]);
+            },
+        }
+    }
+}
